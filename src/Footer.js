97edcee1910c2dfea6c05fc28e121ecd195b9be5
WGCAT <seed9878@naver.com>
2024-12-11 15:47:10 +0900

    (feat): Footer JS 추가

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Footer extends Component{
+    render(){
+        return(<div></div>)
+    }
+}
+
+// 다른 곳에서 import 할 수 있게끔 밖으로 내보내는 export
+export default Footer;
