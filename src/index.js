a52c71b2715f0bd56afdcf1c860824fed99f7b9b
WGCAT <seed9878@naver.com>
2024-12-22 04:18:33 +0900

    (fix): 주석 수정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";  // HashRouter -> BrowserRouter
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
 
-// React 18 이상에서 권장되는 방식으로 createRoot 사용
+// React18 이상에서 createRoot 사용 권장함
 const root = ReactDOM.createRoot(document.getElementById("container"));
 
 root.render(
